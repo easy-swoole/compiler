9c0d779c122b4641145646fc2c0470bdaedc2937
如果的如果 <admin@fosuss.com>
2020-03-24 11:43:01 +0800

    up fix

--- php/Compiler.php
+++ php/Compiler.php
@@ -5,10 +5,38 @@ namespace EasySwoole\Compiler;
 
 
 use EasySwoole\Compiler\Exception\Exception;
+use EasySwoole\Utility\File;
 
 class Compiler
 {
-    static function encrypt(string $file):string
+    static function encrypt(string $file,bool $keepBak = true):int
+    {
+        $count = 0;
+        if(is_dir($file)){
+            $list = File::scanDirectory($file)['files'];
+            if(array($list)){
+                foreach ($list as $file){
+                    $data = self::handleFile($file);
+                    if($keepBak){
+                        $bak = file_get_contents($file);
+                        file_put_contents("{$file}.bak",$bak);
+                    }
+                    file_put_contents($file,$data);
+                    $count++;
+                }
+            }
+        }else{
+            $data = self::handleFile($file);
+            if($keepBak){
+                cpoy($file,"{$file}.bak");
+            }
+            file_put_contents($file,$data);
+            $count++;
+        }
+        return $count;
+    }
+
+    static function handleFile(string $file):string
     {
         if(!file_exists($file)){
             throw new Exception("{$file} not exist");
